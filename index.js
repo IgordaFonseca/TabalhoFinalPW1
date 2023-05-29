efe27f5dcb997e0fe59b265657a14d56b31a3eb4
Igor da Fonseca <igorfonseca-sc@hotmail.com>
2023-05-29 17:47:33 -0300

    inicio da implementação do listar medicos

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function listarPacientes(){
         .then(dados =>{
             let body = document.querySelector("body");
             let tabela = document.createElement("table");
-            tabela.classList.add("table");
+            tabela.classList.add("table", "is-fullwidth");
             let nomeTabela = document.createElement("caption");
-            nomeTabela.classList.add("nomeTabela");
+            nomeTabela.classList.add("nomeTabela","title","is-2");
             nomeTabela.innerHTML="Lista de Pacientes";
-            let cabecalho = document.createElement("th");
+            let cabecalho = document.createElement("tr");
             let tnome = document.createElement("td");
             tnome.innerHTML="Nome";
             let tDataNascimento = document.createElement("td");
@@ -26,6 +26,7 @@ function listarPacientes(){
             cabecalho.append(tnome,tDataNascimento,tDataCadastro,acoes);        
             tabela.append(nomeTabela,cabecalho);
             body.append(tabela);
+            
 
             for(let paciente of dados){
                 let linhaPaciente = document.createElement("tr");
@@ -60,8 +61,11 @@ function listarPacientes(){
 
 function cadastarPaciente(){
     let body = document.querySelector("body");
-    let titulo = document.createElement("h2"); 
+    let titulo = document.createElement("h2");
+    titulo.classList.add("title","is-2")
+    titulo.innerHTML="Cadastrar Paciente"; 
     let form = document.createElement("forms");
+    form.classList.add("form","container","is-fullhd");
     let divNome = document.createElement("div");
     divNome.classList.add("field");
     let labelNome = document.createElement("label");
@@ -88,9 +92,77 @@ function cadastarPaciente(){
     divControDataNascimento.append(dateDataNascimento);
     divDataNascimento.append(labelDataNascimento,divControDataNascimento);
 
-    form.append(divNome,divDataNascimento);
+    let divBotaoCadastrar = document.createElement("div");
+    divBotaoCadastrar.classList.add("control");
+    let botaoCadastrar = document.createElement("button");
+    botaoCadastrar.classList.add("button","is-link");
+    botaoCadastrar.innerHTML="Cadastrar";
+    botaoCadastrar.type="submit";
+    divBotaoCadastrar.append(botaoCadastrar);
+    
+
+    form.append(titulo, divNome,divDataNascimento,divBotaoCadastrar);
     body.append(form);
 
+}
+
+function listarMedicos(){
+    fetch("https://ifsp.ddns.net/webservices/clinicaMedica/medicos")
+        .then(resposta =>{
+            if(!resposta.ok){
+                throw new Error("Houve algum erro"); 
+            }
+            return resposta.json();
+        })
+        .then(dados =>{
+            let body = document.querySelector("body");
+            let tabela = document.createElement("table");
+            tabela.classList.add("table", "is-fullwidth");
+            let nomeTabela = document.createElement("caption");
+            nomeTabela.classList.add("nomeTabela","title","is-2");
+            nomeTabela.innerHTML="Lista de Pacientes";
+            let cabecalho = document.createElement("tr");
+            let tnome = document.createElement("td");
+            tnome.innerHTML="Nome";
+            let tDataCadastro = document.createElement("td");
+            tDataCadastro.innerHTML="Data de cadastro";
+            let acoes = document.createElement("td");
+            acoes.innerHTML="Especialidade";
+
+            cabecalho.append(tnome,tDataCadastro,acoes);        
+            tabela.append(nomeTabela,cabecalho);
+            body.append(tabela);
+            
+
+            /*for(let paciente of dados){
+                let linhaPaciente = document.createElement("tr");
+                let nome = document.createElement("td");
+                nome.innerHTML=paciente.nome;
+                let dataNascimento = document.createElement("td");
+                dataNascimento.innerHTML=paciente.dataNascimento;
+                let dataCadastro = document.createElement("td");
+                dataCadastro.innerHTML=paciente.dataCadastro;
+                let opcoes = document.createElement("td");
+
+                // precisa fazer os eventos listners dos botões
+                let botaoVerConsultas = document.createElement("button");
+                botaoVerConsultas.classList.add("verConsultas","button", "is-primary");
+                botaoVerConsultas.innerHTML="Ver Consulas";
+                let botaoEditar = document.createElement("button");
+                botaoEditar.classList.add("botaoEdita","button","is-warning");
+                botaoEditar.innerHTML="Editar";
+                let botaoDeletar = document.createElement("button");
+                botaoDeletar.classList.add("botaoDeletar", "button", "is-danger")
+                botaoDeletar.innerHTML="Deletar"
+
+                opcoes.append(botaoVerConsultas,botaoEditar,botaoDeletar);
+                linhaPaciente.append(nome, dataNascimento, dataCadastro, opcoes);
+                tabela.append(linhaPaciente);
+            }*/
+        })
+        .catch(erro =>{
+            console.error("Erro encontrado: ",erro);
+        });
 }
 
 let botaoListarPacientes = document.querySelector(".listarPacientes");
@@ -98,3 +170,6 @@ botaoListarPacientes.addEventListener("click",listarPacientes);
 
 let botaoCadastrarPaciente = document.querySelector(".cadastrarPaciente");
 botaoCadastrarPaciente.addEventListener("click",cadastarPaciente);
+
+let botaoListarMedicos = document.querySelector(".listarMedicos");
+botaoListarMedicos.addEventListener("click",listarMedicos);
